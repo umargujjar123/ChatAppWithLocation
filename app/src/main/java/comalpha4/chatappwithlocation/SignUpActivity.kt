ab185ee9e495bae27e9ed205279afa17145e56a1
umar.farooq <umar.farooq@appinsnap.com>
2023-08-06 16:01:12 +0500

    minor

---
+++ app/src/main/java/comalpha4/chatappwithlocation/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package comalpha4.chatappwithlocation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
