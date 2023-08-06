f5b507a902c0ddeb37914ef3e70d066d4f022b26
umar.farooq <umar.farooq@appinsnap.com>
2023-08-06 15:20:39 +0500

    Initial commit

---
+++ app/src/main/java/comalpha4/chatappwithlocation/Models/ChatsModel.kt
@@ -0,0 +1,9 @@
+package comalpha4.chatappwithlocation.Models
+
+data class ChatsModel(
+    val date: String,
+    val message: String,
+    val receiverid: String,
+    val senderid: String,
+    val time: String
+)
